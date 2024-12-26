4166559d13cf2c42d533bd3d358e3060a7db3bf7
LETTTER <3536761163@qq.com>
2024-12-26 21:18:46 +0800

    Site updated: 2024-12-26 21:18:43

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,1 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const textElement = document.createElement('div');
-    textElement.className = 'color-scroll-text';
-    textElement.innerText = '这里是彩色滚动变换的文字内容';
-    document.body.appendChild(textElement);
-});
+ 
